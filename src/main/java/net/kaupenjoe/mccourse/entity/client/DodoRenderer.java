f9a13b976b2508efcff6f472c8058e31b3c744ae
Kaupenjoe <N.Kaupenjohann@gmail.com>
2024-07-28 13:47:11 +0200

    custom entity variants

--- src/main/java/net/kaupenjoe/mccourse/entity/client/DodoRenderer.java
+++ src/main/java/net/kaupenjoe/mccourse/entity/client/DodoRenderer.java
@@ -1,21 +1,32 @@
 package net.kaupenjoe.mccourse.entity.client;
 
+import com.google.common.collect.Maps;
 import net.kaupenjoe.mccourse.MCCourseMod;
 import net.kaupenjoe.mccourse.entity.custom.DodoEntity;
+import net.kaupenjoe.mccourse.entity.variant.DodoVariant;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.Map;
 
 public class DodoRenderer extends MobEntityRenderer<DodoEntity, DodoModel> {
+    private static final Map<DodoVariant, Identifier> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(DodoVariant.class), map -> {
+                map.put(DodoVariant.BLUE, Identifier.of(MCCourseMod.MOD_ID, "textures/entity/dodo/dodo_blue.png"));
+                map.put(DodoVariant.BLACK, Identifier.of(MCCourseMod.MOD_ID, "textures/entity/dodo/dodo_black.png"));
+            });
+
     public DodoRenderer(EntityRendererFactory.Context context) {
         super(context, new DodoModel(context.getPart(ModEntityModelLayers.DODO)), 0.5f);
     }
 
     @Override
     public Identifier getTexture(DodoEntity entity) {
-        return Identifier.of(MCCourseMod.MOD_ID, "textures/entity/dodo/dodo_blue.png");
+        return LOCATION_BY_VARIANT.get(entity.getVariant()); // Identifier.of(MCCourseMod.MOD_ID, "textures/entity/dodo/dodo_blue.png");
     }
 
     @Override
